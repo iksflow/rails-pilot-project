7d629a75677d5f28d824d211bd9a2ec8bf91120b
iksflow <theiksflow@gmail.com>
2021-07-12 14:30:59 +0900

    Initial commit

---
+++ app/controllers/schools_controller.rb
@@ -0,0 +1,41 @@
+class SchoolsController < ApplicationController
+  def new
+    @school = School.new
+  end
+  def create
+    @school = School.new(school_params)
+    @school.save
+    redirect_to schools_index_path
+  end
+  def index
+    @schools = School.all
+  end
+  def show
+    @school = School.find_by_id(params[:id])
+  end
+  def edit
+    @school = School.find_by_id(params[:id])
+  end
+  def update
+    @school = School.find_by_id(params[:id])
+    if @school.update(school_params)
+      redirect_to "/schools"
+    else
+      render "edit"
+    end
+
+  end
+
+  def destroy
+    @school = School.find_by_id(params[:id])
+    @school.destroy
+    redirect_to "/schools"
+    # redirect_to schools_path
+  end
+
+
+  private
+  def school_params
+    params.require(:school).permit(:name)
+  end
+end
